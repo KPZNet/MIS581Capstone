7202a240a5b19229729651dc32766bc93699c310
KPZNET <kpznet@outlook.com>
2021-04-03 19:46:23 -0700

    hourlystation queue

---
+++ Code/Queries/Build_hourlystationqueue_table.sql
@@ -0,0 +1,35 @@
+TRUNCATE hourlystationqueue;
+
+INSERT INTO public.hourlystationqueue(date, hour, source, dest, riders,
+                                         triptime, depart_date, depart_hour)
+
+with trips as
+(select origin,
+        dest,
+        AVG(triptime) as triptime
+from triptimes group by dest, origin)
+
+select hourlyriders.date, hourlyriders.hour, hourlyriders.source, hourlyriders.dest, hourlyriders.riders,
+       trips.triptime,
+
+       CASE
+           WHEN trips.triptime >= 60 AND hourlyriders.hour = 0
+               THEN  cast(hourlyriders.date - interval '1' DAY as Date)
+          ELSE
+               cast(hourlyriders.date as Date)
+
+           END depart_date,
+
+CASE
+           WHEN trips.triptime >= 60 AND hourlyriders.hour = 0
+                THEN  23
+           WHEN trips.triptime >= 60 AND hourlyriders.hour >0
+                THEN hourlyriders.hour - 1
+           ELSE hourlyriders.hour
+
+END depart_hour
+
+from hourlyriders
+INNER JOIN trips ON
+(hourlyriders.source = trips.origin AND hourlyriders.dest = trips.dest)
+
