56a688c7533af3d032e1ba927a63d0883efff9f6
KPZNET <kpznet@outlook.com>
2021-04-05 08:02:25 -0700

    clean up

--- Code/Queries/build_route_times_by_hour.sql
+++
@@ -1,6 +0,0 @@
-with trips as
-         (select origin,
-                 dest,
-                 AVG(triptime)
-          from triptimes group by dest, origin)
-select * from trips where origin = 'PITT' and dest = 'EMBR'
