4b75f552915a58bbd69c6be255de6258803ebf64
KPZNet <KPZNet@outlook.com>
2021-04-12 11:57:24 -0700

    devs

--- Code/BartLibs.py
+++ Code/BartLibs.py
@@ -7,6 +7,61 @@ from datetime import timedelta, date
 import calendar
 import psycopg2
 
+import matplotlib.pyplot as plt
+from statsmodels.graphics import tsaplots
+
+from Code.DataBase import bart
+from statsmodels.tsa.seasonal import seasonal_decompose
+
+def Decomposition(data, per):
+
+    decomposition = seasonal_decompose(data, model="additive", period=per)
+    fig = decomposition.plot()
+    plt.show()
+
+def ACF(data, lags):
+    # Display the autocorrelation plot of your time series
+    fig = tsaplots.plot_acf(data, lags=lags)
+    plt.show()
+    # Display the partial autocorrelation plot of your time series
+    fig = tsaplots.plot_pacf(data, lags=lags)
+    plt.show()
+
+def SumSquares(ft):
+    r =  np.sqrt(  np.square(ft.real) + np.square(ft.imag)  )
+    return r
+
+
+def smoothTriangle(data, degree):
+    triangle=np.concatenate((np.arange(degree + 1), np.arange(degree)[::-1])) # up then down
+    smoothed=[]
+
+    for i in range(degree, len(data) - degree * 2):
+        point=data[i:i + len(triangle)] * triangle
+        smoothed.append(np.sum(point)/np.sum(triangle))
+    # Handle boundaries
+    smoothed=[smoothed[0]]*int(degree + degree/2) + smoothed
+    while len(smoothed) < len(data):
+        smoothed.append(smoothed[-1])
+    return smoothed
+
+def Smooth_1StandardDeviation(dataSet):
+    returnData = []
+    sdv = statistics.stdev(dataSet)
+    mn = statistics.mean(dataSet)
+    Maxthreshold = mn + (2.0 * sdv)
+    Minthreshold = mn - (2.0 * sdv)
+    for d in range(0, len(dataSet)):
+        if (dataSet[d] > Maxthreshold ):
+            print(d, ' : ' ,dataSet[d], mn+sdv)
+            returnData.append(mn + sdv)
+        elif (dataSet[d] < Minthreshold ):
+            print(d, ' : ' ,dataSet[d], mn-sdv)
+            returnData.append(mn - sdv)
+        else:
+            returnData.append(dataSet[d])
+    return returnData
+
 
 def PGBart(query):
     try:
--- Code/BartRiderAnalysis.py
+++ Code/BartRiderAnalysis.py
@@ -6,57 +6,7 @@ from statsmodels.graphics import tsaplots
 from Code.DataBase import bart
 from statsmodels.tsa.seasonal import seasonal_decompose
 
-
-
-def smoothTriangle(data, degree):
-    triangle=np.concatenate((np.arange(degree + 1), np.arange(degree)[::-1])) # up then down
-    smoothed=[]
-
-    for i in range(degree, len(data) - degree * 2):
-        point=data[i:i + len(triangle)] * triangle
-        smoothed.append(np.sum(point)/np.sum(triangle))
-    # Handle boundaries
-    smoothed=[smoothed[0]]*int(degree + degree/2) + smoothed
-    while len(smoothed) < len(data):
-        smoothed.append(smoothed[-1])
-    return smoothed
-
-def Smooth_1StandardDeviation(dataSet):
-    returnData = []
-    sdv = statistics.stdev(dataSet)
-    mn = statistics.mean(dataSet)
-    Maxthreshold = mn + (2.0 * sdv)
-    Minthreshold = mn - (2.0 * sdv)
-    for d in range(0, len(dataSet)):
-        if (dataSet[d] > Maxthreshold ):
-            print(d, ' : ' ,dataSet[d], mn+sdv)
-            returnData.append(mn + sdv)
-        elif (dataSet[d] < Minthreshold ):
-            print(d, ' : ' ,dataSet[d], mn-sdv)
-            returnData.append(mn - sdv)
-        else:
-            returnData.append(dataSet[d])
-    return returnData
-
-def Decomposition(data, per):
-
-    decomposition = seasonal_decompose(data, model="additive", period=per)
-    fig = decomposition.plot()
-    plt.show()
-
-def ACF(data):
-    # Display the autocorrelation plot of your time series
-    fig = tsaplots.plot_acf(data, lags=24)
-    plt.show()
-    # Display the partial autocorrelation plot of your time series
-    fig = tsaplots.plot_pacf(data, lags=24)
-    plt.show()
-
-
-
-def SumSquares(ft):
-    r =  np.sqrt(  np.square(ft.real) + np.square(ft.imag)  )
-    return r
+import BartLibs
 
 
 def BARRunFFT():
@@ -79,10 +29,10 @@ def BARRunFFT():
     """
     dat = bart.PGBartLocal(query)
     plotdata = list(map(lambda x: x[0], dat))
-    smoothData = Smooth_1StandardDeviation(plotdata)
+    smoothData = BartLibs.Smooth_1StandardDeviation(plotdata)
 
-    Decomposition(smoothData, 5)
-    ACF(smoothData)
+    BartLibs.Decomposition(smoothData, 5)
+    BartLibs.ACF(smoothData)
 
     datasize = len(smoothData)
     x = list(range(datasize))
@@ -103,7 +53,7 @@ def BARRunFFT():
     print(statistics.mean(smoothData))
     ft = np.fft.fft(smoothData)
     rt = []
-    rt = list(map(lambda x: SumSquares(x), ft))
+    rt = list(map(lambda x: BartLibs.SumSquares(x), ft))
     le = len(rt)
     scal = 2 / le
     rt = list(map(lambda x: scal * x, rt))
@@ -114,7 +64,7 @@ def CosFFT():
     N = 256
     T = 1/N
     x = np.linspace(0.0, N*T, N, endpoint=False)
-    y = 10*np.sin(20 * 2.0*np.pi*x) #+ 5*np.sin(10 * 2.0*np.pi*x)
+    y = 10*np.sin(25.6 * 2.0*np.pi*x) #+ 5*np.sin(10 * 2.0*np.pi*x)
     y = list(map(lambda x: x - statistics.mean(y), y))
     plt.plot(x, y,
              color='blue',
@@ -124,15 +74,15 @@ def CosFFT():
 
     ft = np.fft.fft(y)
     rt = []
-    rt = list(map(lambda x: SumSquares(x), ft))
+    rt = list(map(lambda x: BartLibs.SumSquares(x), ft))
     le = len(rt)
     scal = 2 / le
     rt = list(map(lambda x: scal * x, rt))
     plt.plot(rt[:128])
     plt.show()
 
-    Decomposition(y, 51)
-    ACF(y)
+    BartLibs.Decomposition(y, 10)
+    BartLibs.ACF(y, 10)
 
 
 def TryDecomp():
@@ -141,7 +91,7 @@ def TryDecomp():
     x = np.linspace(0.0, N*T, N, endpoint=False)
     y = 10*np.sin(5 * 2.0*np.pi*x) + 0.5*np.sin(10 * 2.0*np.pi*x)
     y = list(map(lambda x: x - statistics.mean(y), y))
-    Decomposition(y,51)
+    BartLibs.Decomposition(y,51)
 
 try:
     #BARRunFFT()
