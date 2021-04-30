c0b25b7f2a677dc94ae3980de0b54a26486f9184
KPZNET <kpznet@outlook.com>
2021-04-29 17:51:07 -0700

    Alienware

--- Code/BartRiderAnalysis.py
+++ Code/BartRiderAnalysis.py
@@ -17,14 +17,14 @@ try:
     # BARTPlots.RunBARTTimeSeries2("CONC", 7, 2019)
     #
     #BARTPlots.PlotRidersOnMap(2019)
-    #BARTPlots.PlotRidersOnMapTo(2019)
+    BARTPlots.PlotRidersOnMapTo(2019)
 
     # BARTPlots.PlotYearlySumRidersPerOrigin("CONC", 2019)
 
-    # BARTPlots.CompareMultiDayRidersToYearlyAveFrom(date(2019,1, 1),date(2019, 3, 1),'CONC', 7, 2019, 1, 5, 1)
+    BARTPlots.CompareMultiDayRidersToYearlyAveFrom(date(2019,4, 1),date(2019, 5, 1),'CONC', 7, 2019, 1, 5, 1)
     #BARTPlots.CompareMultiDayRidersToYearlyAveDest(date(2019, 12, 1),date(2019, 12, 30),'19TH', 8, 2019, 1, 5, 1)
 
-    BARTPlots.CompareMultipleDayRidersFrom ( date ( 2019, 1, 1 ), date ( 2019, 12, 30 ), 'CONC', 7, 15, 5, 0, 28 )
+    BARTPlots.CompareMultipleDayRidersFrom ( date ( 2019, 4, 1 ), date ( 2019, 5, 1 ), 'CONC', 7, 15, 5, 0, 1 )
     #BARTPlots.CompareMultipleDayRidersTo(date(2019, 1, 1),date(2019, 12, 30), '19TH', 8, 20, 5, 0,1)
     #BARTPlots.PlotTotalRidersByHour(2019)
     #BARTPlots.CompareRidersPerISODOW(2019)
